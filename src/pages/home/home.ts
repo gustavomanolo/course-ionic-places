baf4e4087a97046fc893d13fb931bdcf76ff6c03
Gustavo Manolo <gustavomanolo@gmail.com>
2019-01-07 11:49:45 -0600

    updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,12 +4,16 @@ import { LugarPage } from "../lugar/lugar";
 import { PlaceAddPage } from "../place-add/place-add";
 import { PlacesService } from "../../services/places.service";
 
+declare let cordova: any;
+
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
 export class HomePage {
   places: any = [];
+  cordova: any;
+
   constructor(
     public navCtrl: NavController,
     public placesServices: PlacesService
@@ -20,6 +24,32 @@ export class HomePage {
       .subscribe(placesFB => {
         this.places = placesFB;
       });
+  }
+
+  ionViewDidLoad() {
+    if (typeof cordova !== "undefined") {
+      alert(JSON.stringify(cordova));
+      if (typeof window !== "undefined" && typeof window['plugins'] !== "undefined") {
+        console.log(window['plugins'].acr122);
+
+        // Wait for NFC read
+        window['plugins']['acr122'].listen(function(event) {
+          console.log("listen!");
+          if (event.state == "Present") {
+            alert("Scanned tag with id " + event.tag.id);
+          }
+        });
+      }
+    }
+    // console.log('-> typeof', typeof acr122)
+    // console.log("inbox loaded");
+    // var nfc = window.acr122;
+
+    // nfc.listen(function(event) {
+    //   if (event.state == "Present") {
+    //     alert("Scanned tag with id " + event.tag.id);
+    //   }
+    // });
   }
 
   // goToLugar(name) {
